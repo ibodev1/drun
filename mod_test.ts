745320504ed58c74a1ccbc0f40a404d4283478b2
İbrahim Ödev <74017534+ibodev1@users.noreply.github.com>
2023-04-19 15:04:10 +0300

    test

--- mod_test.ts
+++ mod_test.ts
@@ -1,6 +1,5 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import { mode } from "./mod.ts";
 
-Deno.test("test starter function", async (): Promise<void> => {
-  assertEquals(mode(), 0);
+Deno.test("drun cli test", (): void => {
+  assertEquals(true, !0);
 });
