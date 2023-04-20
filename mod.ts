0f23703db0eff091746014e7aede2443bac48dd0
Ibrahim Odev <ibrahimodev42@gmail.com>
2023-04-20 20:35:07 +0300

    update project.yml sytnax

--- mod.ts
+++ mod.ts
@@ -1,15 +1,13 @@
 import * as path from "path";
 import { projectFile, runCmd } from "./src/utils.ts";
-import { getTaskIndex } from "./src/task.ts";
 
 export async function drux(taskName: string) {
   const projectFilePath = path.join(Deno.cwd(), "./project.yml");
   const projectFileContent = await projectFile(projectFilePath);
   const tasks = projectFileContent?.tasks ?? null;
   if (!!taskName && !!tasks) {
-    const taskIndex = getTaskIndex(tasks);
-    if (taskIndex >= 0) {
-      const task = tasks[taskIndex][taskName];
+    const task = tasks[taskName];
+    if (task) {
       await runCmd(task.split(" "), Deno.cwd());
     }
   }
