66b6b2ba967db10e67dc66eee2ae12ea38529aad
Ibrahim Odev <ibrahimodev42@gmail.com>
2023-04-21 12:42:03 +0300

    update runCmd func

--- src/utils.ts
+++ src/utils.ts
@@ -22,50 +22,58 @@ export async function denoFile(denoFilePath: string) {
     }
   } catch (error) {
     if (error instanceof Deno.errors.NotFound) {
-      console.error(errorMessage("deno.json does not exist in root directory."));
+      console.error(
+        errorMessage("deno.json does not exist in root directory."),
+      );
     } else if (error instanceof Deno.errors.PermissionDenied) {
-      console.error(errorMessage("Need `--allow-read` and `--allow-run` permissions."));
+      console.error(
+        errorMessage("Need `--allow-read` and `--allow-run` permissions."),
+      );
     } else {
       throw error;
     }
   }
 }
 
-export const runCmd = async (cmd: string[], cwd: string): Promise<number> => {
-  const taskName = getTaskName();
-  const isWindows = os.platform() === "windows";
-  if (isWindows) {
-    const windowsProps = ["/c", "cmd"];
-    for (let i = 0; i < 2; i++) {
-      cmd.unshift(windowsProps[i]);
+export const runCmd = async (cmd: string[], cwd: string): Promise<void> => {
+  try {
+    const taskName = getTaskName();
+    const isWindows = os.platform() === "windows";
+    if (isWindows) {
+      const windowsProps = ["/c", "cmd"];
+      for (let i = 0; i < 2; i++) {
+        cmd.unshift(windowsProps[i]);
+      }
     }
+
+    const runCmd = Deno.run({
+      cmd,
+      cwd,
+      stdout: "inherit",
+      stderr: "inherit",
+      stdin: "inherit",
+    });
+
+    const realCommand = isWindows
+      ? cmd.splice(2, cmd.length - 2).join(" ")
+      : cmd.join(" ");
+    const startLog = logger(realCommand) + "\n";
+    const startLogUnit8 = new TextEncoder().encode(startLog);
+    await Deno.stdout.write(startLogUnit8);
+
+    const { code } = await runCmd.status();
+
+    if (code !== 0) {
+      const errorLogUnit8 = new TextEncoder().encode(
+        errorMessage(realCommand, taskName),
+      );
+      await Deno.stdout.write(errorLogUnit8);
+    }
+    
+    runCmd.close();
+
+    Deno.exit(code);
+  } catch (error) {
+    console.error(errorMessage(error.toString()));
   }
-
-  const runCmd = Deno.run({
-    cmd,
-    cwd,
-    stdout: "inherit",
-    stderr: "inherit",
-    stdin: "inherit",
-  });
-
-  const realCommand = isWindows
-    ? cmd.splice(2, cmd.length - 2).join(" ")
-    : cmd.join(" ");
-  const startLog = logger(realCommand) + "\n";
-  const startLogUnit8 = new TextEncoder().encode(startLog);
-  await Deno.stdout.write(startLogUnit8);
-
-  const { code } = await runCmd.status();
-
-  if (code !== 0) {
-    const errorLogUnit8 = new TextEncoder().encode(
-      errorMessage(realCommand, taskName),
-    );
-    await Deno.stdout.write(errorLogUnit8);
-  }
-
-  runCmd.close();
-
-  Deno.exit(code);
 };
