19343a34b6be8fecaf76f41e771f25570084b2e5
Ibrahim Odev <ibrahimodev42@gmail.com>
2023-04-23 18:59:34 +0300

    update: file exisist

--- mod.ts
+++ mod.ts
@@ -1,27 +1,33 @@
 import { runCmd } from "./src/cmd.ts";
-import { denoFile } from "./src/utils/file.ts";
+import { denoFile, exists } from "./src/utils/file.ts";
 import { errorMessage } from "./src/utils/logs.ts";
 import * as color from "https://deno.land/std@0.184.0/fmt/colors.ts";
 
-export async function drux(taskName: string | null, configFile: string | null = "deno.json") {
+export async function drux(
+  taskName: string | null,
+  configFile: string | null = "deno.json",
+) {
   try {
-    const denoFilePath = await Deno.realPath(configFile ?? "deno.json");
-    const denoFileContent = await denoFile(denoFilePath);
-    const tasks = denoFileContent?.tasks ?? null;
-    if (!!taskName && !!tasks) {
-      const task = tasks[taskName];
-      if (task && task.trim() !== "") {
-        await runCmd(task.split(" "), Deno.cwd());
-      }
-    } else {
-      if (tasks && Array.isArray(Object.entries(tasks))) {
-        console.info(color.green("DRux Available tasks:"));
-        const entries = Object.entries(tasks) ?? [];
-        for (let i = 0; i < entries.length; i++) {
-          const key = entries[i][0];
-          const task = entries[i][1];
-          console.info("- " + color.cyan(key.toString()));
-          console.log("   " + task?.toString())
+    const fileExists = await exists(configFile ?? "deno.json");
+    if (fileExists) {
+      const denoFilePath = await Deno.realPath(configFile ?? "deno.json");
+      const denoFileContent = await denoFile(denoFilePath);
+      const tasks = denoFileContent?.tasks ?? null;
+      if (!!taskName && !!tasks) {
+        const task = tasks[taskName];
+        if (task && task.trim() !== "") {
+          await runCmd(task.split(" "), Deno.cwd());
+        }
+      } else {
+        if (tasks && Array.isArray(Object.entries(tasks))) {
+          console.info(color.green("DRux Available tasks:"));
+          const entries = Object.entries(tasks) ?? [];
+          for (let i = 0; i < entries.length; i++) {
+            const key = entries[i][0];
+            const task = entries[i][1];
+            console.info("- " + color.cyan(key.toString()));
+            console.log("   " + task?.toString());
+          }
         }
       }
     }
@@ -29,10 +35,6 @@ export async function drux(taskName: string | null, configFile: string | null = 
     if (error instanceof Deno.errors.PermissionDenied) {
       console.error(
         errorMessage("Need `--allow-read` and `--allow-run` permissions."),
-      );
-    } else if (error instanceof Deno.errors.NotFound) {
-      console.error(
-        errorMessage(`${configFile ?? "deno.json"} does not exist in root directory.\nPath : ${Deno.cwd()}`),
       );
     } else {
       throw error;
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,20 +1,16 @@
 import { errorMessage } from './logs.ts';
 
-export async function denoFile(denoFilePath: string) {
+async function denoFile(denoFilePath: string) {
   try {
-    const fileInfo = await Deno.stat(denoFilePath);
-    if (fileInfo.isFile) {
+    const fileExists = await exists(denoFilePath);
+    if (fileExists) {
       const denoFile = await Deno.readTextFile(denoFilePath);
       return JSON.parse(denoFile);
     } else {
       return null;
     }
   } catch (error) {
-    if (error instanceof Deno.errors.NotFound) {
-      console.error(
-        errorMessage("deno.json does not exist in root directory."),
-      );
-    } else if (error instanceof Deno.errors.PermissionDenied) {
+    if (error instanceof Deno.errors.PermissionDenied) {
       console.error(
         errorMessage("Need `--allow-read` and `--allow-run` permissions."),
       );
@@ -23,3 +19,22 @@ export async function denoFile(denoFilePath: string) {
     }
   }
 }
+
+// Exists file
+// Github : https://github.com/BentoumiTech/denox/blob/bc6cfdfb30fa2d130c5d1963350420229c47e912/src/utils/file.ts#L29
+async function exists(filename: string | null = "deno.json"): Promise<boolean> {
+  try {
+    const fileInfo = await Deno.stat(filename ?? "deno.json");
+    return fileInfo?.isFile ?? true;
+  } catch (error) {
+    if (error instanceof Deno.errors.NotFound) {
+      console.error(
+        errorMessage(`${filename ?? "deno.json"} does not exist in root directory.`),
+      );
+      return false;
+    }
+    throw error;
+  }
+}
+
+export { denoFile, exists }
